0ae09bf23e3c41ebbb603f73ed8633bd7b03f664
nikhil-ucer <nikhil.17.nb@gmail.com>
2026-03-10 07:38:55 +0000

    changes day 4

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Demo from './Components/Demo'
+
 
 function App() {
 
@@ -9,7 +11,10 @@ function App() {
     <div>
       <h1>Hello! My name is Nikhil</h1>
       <Employee></Employee>
+      <Test></Test>
+      <Demo></Demo>
       
+
     </div>
   )
 }
@@ -19,6 +24,13 @@ function Employee(){
     <h1>Nikhil Barua</h1>
   )
 }
+
+function Test(){
+  return(
+    <h1>Hello This is second component</h1>
+  )
+}
+
 
 
 
---
+++ my-react-app/src/Components/Demo.jsx
@@ -0,0 +1,9 @@
+function Demo(){
+    return(
+        <h1>This an example Demo</h1>
+    )
+}
+
+
+
+export default Demo
