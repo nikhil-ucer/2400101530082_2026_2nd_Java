5916b491928202f73356e66d5ced601d759d0e65
nikhil-ucer <nikhil.17.nb@gmail.com>
2026-04-07 07:36:13 +0000

    state hooks

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,30 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Demo from './Components/Demo'
-
 
 function App() {
+  let data = "Demo"
+  function updata(){
+    data = "Example"
+    alert(data)
+  }
 
-  function callfun(){
-            alert("Function Call");
-        }
-  function callApple(){
-            alert("You Clicked on Apple Button");
-        }
-  function callBanana(){
-            alert("You Clicked on Apple Button");
-        }            
-
+ // facing problem for variable state updatation => so we use react state hooks
   return (
     <div>
-      <h1>Welcome to New Project</h1>
-      <button onClick={callfun}>OK</button>
-      <button onClick={callApple}>Apple</button>
-      <button onClick={callBanana}>Banana</button>
-      
-
+      <h1>Example of arrow function</h1>
+      <h1>{data}</h1>
+      <button onClick={updata}>Click to Update</button>
     </div>
   )
 }
